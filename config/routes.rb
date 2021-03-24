61dafb309b2d5ac164434df0ee6d6498139822c8
Hiroya Yabiku <h_yabiku@atoj.co.jp>
2021-03-24 22:00:37 +0900

    productモデルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root 'admin#index'
+  # root 'admin#index'
+  get 'admin', to: 'admin#index'
+  get 'admin/new', to: 'admin#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
