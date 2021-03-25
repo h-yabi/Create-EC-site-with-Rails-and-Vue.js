0e8974761543c1aad2a9323d06332a45076fb3ef
Hiroya Yabiku <h_yabiku@atoj.co.jp>
2021-03-25 18:02:59 +0900

    binding.pry導入、form送信用のメソッド定義

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,19 @@
 class AdminController < ApplicationController
   def index
   end
-  def index
+
+  def new
+    @product = Product.new
   end
+
+  def create
+    Product.create(product_params)
+    binding.pry
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :image, :price)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # root 'admin#index'
   get 'admin', to: 'admin#index'
   get 'admin/new', to: 'admin#new'
+  post 'admin', to: 'admin#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
